771759cdfd59cb0fba5262cbd228ebef074de229
kevindahub <kevin.van.houtte1@gmail.com>
2016-03-14 13:01:56 +0100

    Hal browser working, Dummy data added and validation constraints on entity

--- src/main/java/be/ordina/ordineo/model/projection/EmployeeProjection.java
+++ src/main/java/be/ordina/ordineo/model/projection/EmployeeProjection.java
@@ -1,10 +1,29 @@
 package be.ordina.ordineo.model.projection;
 
 import be.ordina.ordineo.model.Employee;
+import be.ordina.ordineo.model.Gender;
+import be.ordina.ordineo.model.Unit;
 import org.springframework.data.rest.core.config.Projection;
+
+import java.time.LocalDate;
 
 @Projection(name="employeeProjection", types=Employee.class)
 public interface EmployeeProjection {
 
     String getUsername();
+    String getFirstName();
+    String getLastName();
+    String getLinkedin();
+    String getEmail();
+    String getPhoneNumber();
+    String getFunction();
+    Unit getUnit();
+    String getDescription();
+    String getProfilePicture();
+    Gender getGender();
+    LocalDate getBirthDate();
+    LocalDate getHireDate();
+    LocalDate getStartDate();
+    LocalDate getResignationDate();
+
 }
