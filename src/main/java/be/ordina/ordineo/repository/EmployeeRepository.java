5ad90805e3eb2158c34b8e834c3e99f69c9f0177
shadi behrooz abedini <shbe@ORD01752.local>
2016-05-03 14:18:05 +0200

    fix

--- src/main/java/be/ordina/ordineo/repository/EmployeeRepository.java
+++ src/main/java/be/ordina/ordineo/repository/EmployeeRepository.java
@@ -9,11 +9,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-<<<<<<< HEAD
-=======
-import org.springframework.hateoas.Resources;
->>>>>>> cf01e117052c356f4c46a7d1b95f99cc940935bf
-import org.springframework.security.access.prepost.PreAuthorize;
 
 
 
