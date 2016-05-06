b9d7c6fafcc1c8e5a8aeb28905cfac647330de1f
Philippe Damen <mr.deleeuw@gmail.com>
2016-05-06 13:35:00 +0200

    Augmented test coverage (#31)
    * Augmented test coverage
    
    * Augmented test coverage+ authtoken in tests

--- src/test/java/be/ordina/ordineo/employee/rest/EmployeeRestTest.java
+++ src/test/java/be/ordina/ordineo/employee/rest/EmployeeRestTest.java
@@ -2,12 +2,15 @@ package be.ordina.ordineo.employee.rest;
 
 
 import be.ordina.ordineo.EmployeeCoreApplication;
+import be.ordina.ordineo.filter.JwtFilter;
 import be.ordina.ordineo.model.Employee;
 import be.ordina.ordineo.model.Gender;
 import be.ordina.ordineo.model.Unit;
 import be.ordina.ordineo.repository.EmployeeRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import io.jsonwebtoken.MalformedJwtException;
+import org.codehaus.jettison.json.JSONObject;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -27,30 +30,26 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.WebApplicationContext;
 
-import javax.servlet.Filter;
+import javax.servlet.ServletException;
 import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
-
-
 
 import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.halLinks;
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.snippet.Attributes.attributes;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.restdocs.snippet.Attributes.key;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -66,6 +65,8 @@ public class EmployeeRestTest {
 
     private ObjectWriter objectWriter;
 
+    private String authToken;
+
     @Autowired
     private ObjectMapper objectMapper;
 
@@ -74,17 +75,59 @@ public class EmployeeRestTest {
     @Autowired
     private WebApplicationContext wac;
     private RestDocumentationResultHandler document;
-    @Autowired
-    private Filter springSecurityFilterChain;
 
 
     @Before
-    public void setup() {
+    public void setup() throws Exception{
         this.document = document("{method-name}");
         mockMvc = MockMvcBuilders.webAppContextSetup(wac)
-                .apply(documentationConfiguration(this.restDocumentation).uris().withScheme("https")).alwaysDo(this.document).addFilter(springSecurityFilterChain)
+                .apply(documentationConfiguration(this.restDocumentation).uris().withScheme("https")).alwaysDo(this.document)
+                .addFilter(new JwtFilter(), "/*")
                 .build();
         objectWriter = objectMapper.writer();
+
+        authToken = getAuthToken();
+    }
+
+    public String getAuthToken() throws Exception {
+
+        String url = "https://gateway-ordineo.cfapps.io/auth";
+        URL object = new URL(url);
+
+        HttpURLConnection con = (HttpURLConnection) object.openConnection();
+        con.setDoOutput(true);
+        con.setDoInput(true);
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+        con.setRequestMethod("POST");
+
+        JSONObject cred = new JSONObject();
+        JSONObject auth = new JSONObject();
+        JSONObject parent = new JSONObject();
+
+        cred.put("username", "Nivek");
+        cred.put("password", "password");
+
+        OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+        wr.write(cred.toString());
+        wr.flush();
+
+        //display what returns the POST request
+
+        StringBuilder sb = new StringBuilder();
+        int HttpResult = con.getResponseCode();
+        if (HttpResult == HttpURLConnection.HTTP_OK) {
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(con.getInputStream(), "utf-8"));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            br.close();
+            return "Bearer " +sb.substring(10,sb.length()-3);
+        } else {
+            return con.getResponseMessage();
+        }
     }
 
     @Test
@@ -113,7 +156,7 @@ public class EmployeeRestTest {
 
        mockMvc.perform(
                 get("/employees/1").accept(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJOaXZlayIsInJvbGUiOiJbUk9MRV9VU0VSLCBST0xFX0FETUlOXSIsImNyZWF0ZWQiOjE0NjIxNzI4Njk5ODQsImV4cCI6MTQ2Mjc3NzY2OX0.BFpbs12BCKHvju7ICzmzG8_tnfM1AwLGoTF56u3i8ZAR_A56gvivGaL1uKSjkK4HXBcMt_NjAdnFubx-uoSQ8Q"))
+                .header("Authorization", authToken))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.username", is("Nivek")))
                 .andExpect(jsonPath("$.firstName", is("Kevin")))
@@ -121,6 +164,7 @@ public class EmployeeRestTest {
                 .andExpect(jsonPath("$.email", is("kevin@gmail.com")))
                 .andExpect(jsonPath("$.phoneNumber", is("047637287")))
                 .andExpect(jsonPath("$.function", is("Software Developer Java")))
+                .andExpect(jsonPath("$.unit.name", is("JWorks")))
                 .andExpect(jsonPath("$.description", is("Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters")))
                 .andExpect(jsonPath("$.gender", is("MALE")))
                 .andExpect(jsonPath("$.birthDate", is("1992-07-25")))
@@ -149,7 +193,7 @@ public class EmployeeRestTest {
                 ));
 
         mockMvc.perform(get("/employees/search/employee?username=Nivek&projection=aboutProjection")
-                .header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJOaXZlayIsInJvbGUiOiJbUk9MRV9VU0VSLCBST0xFX0FETUlOXSIsImNyZWF0ZWQiOjE0NjIxNzI4Njk5ODQsImV4cCI6MTQ2Mjc3NzY2OX0.BFpbs12BCKHvju7ICzmzG8_tnfM1AwLGoTF56u3i8ZAR_A56gvivGaL1uKSjkK4HXBcMt_NjAdnFubx-uoSQ8Q"))
+                .header("Authorization", authToken))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.username", is("Nivek")))
                 .andExpect(jsonPath("$.firstName", is("Kevin")))
@@ -178,7 +222,7 @@ public class EmployeeRestTest {
                 ));
 
         mockMvc.perform(get("/employees/search/employeeName?name=kevin&projection=searchProjection")
-                .header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJOaXZlayIsInJvbGUiOiJbUk9MRV9VU0VSLCBST0xFX0FETUlOXSIsImNyZWF0ZWQiOjE0NjIxNzI4Njk5ODQsImV4cCI6MTQ2Mjc3NzY2OX0.BFpbs12BCKHvju7ICzmzG8_tnfM1AwLGoTF56u3i8ZAR_A56gvivGaL1uKSjkK4HXBcMt_NjAdnFubx-uoSQ8Q"))
+                .header("Authorization", authToken))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$._embedded.employees[0].username", is("Nivek")))
                 .andExpect(jsonPath("$._embedded.employees[0].firstName", is("Kevin")))
@@ -194,7 +238,8 @@ public class EmployeeRestTest {
         employee.setFirstName("Ken");
         String string = objectWriter.writeValueAsString(employee);
 
-        mockMvc.perform(put("/employees/" +employee.getId()).content(string).contentType(APPLICATION_JSON)).andExpect(status().isNoContent());
+        mockMvc.perform(put("/employees/" +employee.getId()).content(string).contentType(APPLICATION_JSON).header("Authorization", authToken))
+        .andExpect(status().isNoContent());
     }
 
     @Test
@@ -203,7 +248,8 @@ public class EmployeeRestTest {
         employee.setFirstName(null);
         String string = objectWriter.writeValueAsString(employee);
 
-        mockMvc.perform(put("/employees/" +employee.getId()).content(string).contentType(APPLICATION_JSON)).andExpect(status().isBadRequest());
+        mockMvc.perform(put("/employees/" +employee.getId()).content(string).contentType(APPLICATION_JSON).header("Authorization", authToken))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
@@ -211,6 +257,10 @@ public class EmployeeRestTest {
         Employee employee = employeeRepository.findByUsernameIgnoreCase("Nivek");
         employee.setId(null);
         employee.setUsername("Keloggs");
+        Unit unit = new Unit();
+        unit.getId();
+        unit.setName("TestUnit");
+        employee.setUnit(unit);
         String string = objectWriter.writeValueAsString(employee);
 
         ConstrainedFields fields = new ConstrainedFields(Employee.class);
@@ -233,14 +283,16 @@ public class EmployeeRestTest {
                         fields.withPath("resignationDate").description("The employee's resignation date").type(LocalDate.class)
                        ));
 
-        mockMvc.perform(post("/employees").content(string).contentType(MediaTypes.HAL_JSON)).andExpect(status().isCreated()).andReturn().getResponse().getHeader("Location");
+        mockMvc.perform(post("/employees").content(string).contentType(MediaTypes.HAL_JSON).header("Authorization", authToken))
+                .andExpect(status().isCreated()).andReturn().getResponse().getHeader("Location");
     }
     @Test
     public void postEmployeeWithDuplicateUsername() throws Exception {
         Employee employee = employeeRepository.findByUsernameIgnoreCase("Nivek");
         String string = objectWriter.writeValueAsString(employee);
 
-        mockMvc.perform(post("/employees/").content(string).contentType(APPLICATION_JSON)).andExpect(status().isConflict());
+        mockMvc.perform(post("/employees/").content(string).contentType(APPLICATION_JSON).header("Authorization", authToken))
+                .andExpect(status().isConflict());
     }
 
     @Test
@@ -251,7 +303,8 @@ public class EmployeeRestTest {
         employee.setFirstName(null);
         String string = objectWriter.writeValueAsString(employee);
 
-        mockMvc.perform(post("/employees/").content(string).contentType(APPLICATION_JSON)).andExpect(status().isBadRequest());
+        mockMvc.perform(post("/employees/").content(string).contentType(APPLICATION_JSON).header("Authorization", authToken))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
@@ -259,8 +312,27 @@ public class EmployeeRestTest {
         Employee employee = employeeRepository.findByUsernameIgnoreCase("Nivek");
         employee.setId(null);
         String string = objectWriter.writeValueAsString(employee);
-        mockMvc.perform(put("/linkedin").content(string).contentType(APPLICATION_JSON)).andExpect(status().isAccepted());
+        mockMvc.perform(put("/linkedin").content(string).contentType(APPLICATION_JSON).header("Authorization", authToken))
+                .andExpect(status().isAccepted());
     }
+
+    @Test(expected = ServletException.class)
+    public void missingHeaderTest() throws Exception {
+        Employee employee = employeeRepository.findByUsernameIgnoreCase("Nivek");
+        employee.setId(null);
+        String string = objectWriter.writeValueAsString(employee);
+        mockMvc.perform(put("/linkedin").content(string).contentType(APPLICATION_JSON));
+    }
+
+    @Test(expected = ServletException.class)
+    public void invalidTokenTest() throws Exception {
+        Employee employee = employeeRepository.findByUsernameIgnoreCase("Nivek");
+        employee.setId(null);
+        String string = objectWriter.writeValueAsString(employee);
+        mockMvc.perform(put("/linkedin").content(string).contentType(APPLICATION_JSON).header("Authorization", "Bearer eyJhbGciOitetreriJ9.eyJzdWIiOiJOaXZlayIsInJvbGUiOiJbUk9MRV9VU0VSLCBST0xFX0FETUlOXSIsImNyZWF0ZWQiOjE0NjIxNzI4Njk5ODQsImV4cCI6MTQ2Mjc3NzY2OX0.BFpbs12BCKHvju7ICzmzG8_tnfM1AwLGoTF56u3i8ZAR_A56gvivGaL1uKSjkK4HXBcMt_NjAdnFubx-uoSQ8Q"))
+        ;
+    }
+
 
     private static class ConstrainedFields {
         private final ConstraintDescriptions constraintDescriptions;
