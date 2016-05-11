68087c0454895aff0cb96f63422f1e1978121a92
shadibehroozabedini <Shadi.BehroozAbedini@ordina.be>
2016-05-11 10:50:29 +0200

    Feature/authorization (#32)
    * adding filter
    
    * adding filter
    
    * adding securityconfig
    
    * commenting PreAuthorize annotation
    
    * fix
    
    * authorization added
    
    * adding test util

--- src/main/java/be/ordina/ordineo/repository/EmployeeRepository.java
+++ src/main/java/be/ordina/ordineo/repository/EmployeeRepository.java
@@ -1,7 +1,6 @@
 package be.ordina.ordineo.repository;
 
 import be.ordina.ordineo.model.Employee;
-import be.ordina.ordineo.model.projection.EmployeeProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
@@ -9,8 +8,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-
-
+import org.springframework.security.access.prepost.PreAuthorize;
 
 
 import java.util.List;
@@ -18,8 +16,7 @@ import java.util.List;
 
 @RepositoryRestResource
 
-//@PreAuthorize("hasRole('ROLE_USER')")
-
+@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER')")
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee,Long>{
 
     @RestResource(path="employee",rel="employee")
@@ -29,9 +26,9 @@ public interface EmployeeRepository extends PagingAndSortingRepository<Employee,
     @RestResource(path="unit",rel="unit")
     Page<Employee> findByUnitName(@Param("unit")String unit,@Param("page")Pageable pageable);
 
+
     @RestResource(path = "employeeName",rel = "searchEmployee")
     @Query("select DISTINCT o from Employee o where lower(o.firstName) LIKE CONCAT('%',lower(:name),'%') or lower(o.lastName) LIKE CONCAT('%',lower(:name),'%') ")
     List<Employee> findByFirstNameOrLastName(@Param("name") String name);
-
 
 }
